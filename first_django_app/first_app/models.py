81d85e74ba81396564d82cf1072bc0f8ce8a06ec
chimaek <pipiru100@gmail.com>
2022-06-20 15:31:39 +0900

    add

--- first_django_app/first_app/models.py
+++ first_django_app/first_app/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db.models import *
 
-# Create your models here.
+from datetime import datetime
+
+#모델 추가시  py manage.py migrations <app name >
+
+class Question(Model):
+    question = CharField(max_length=200)
+    pub_date = DateTimeField(auto_now=True, name="Date Published")
+
+
+class Choice(Model):
+    question = ForeignKey(Question, on_delete=CASCADE)
+    choice_text = CharField(max_length=100)
+    votes = IntegerField(default=0)
--- first_django_app/first_app/views.py
+++ first_django_app/first_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from first_app.models import Question
+
+
+def index(req):
+    lust = Question.objects.order_by('Date Published')[:5]
+    return render(req, 'index.html', {'date': lust})
