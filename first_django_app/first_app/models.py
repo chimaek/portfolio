75bcf696bb43342f312eec3cf9d5adef76e525df
chimaek <pipiru100@gmail.com>
2022-06-21 18:31:24 +0900

    add

--- first_django_app/first_app/models.py
+++ first_django_app/first_app/models.py
@@ -1,16 +1,26 @@
 from django.db.models import *
 
-from datetime import datetime
-
+from datetime import datetime, timedelta
 
 # 모델 추가시  py manage.py migrations <app name >
+from django.utils import timezone
+
 
 class Question(Model):
     question = CharField(max_length=200)
-    pub_date = DateTimeField(name="Date_published")
+    pub_date = DateTimeField(name="pub_date")
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+
+    def __str__(self):
+        return self.question
 
 
 class Choice(Model):
     question = ForeignKey(Question, on_delete=CASCADE)
     choice_text = CharField(max_length=100)
     votes = IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- first_django_app/first_app/views.py
+++ first_django_app/first_app/views.py
@@ -6,4 +6,19 @@ from first_app.models import Question
 
 def index(req):
     lust = Question.objects.order_by('pub_date')[:5]
-    return render(req, 'index.html', {'date': lust})
+    # output = ','.join([q.question for q in lust])
+    # return render(req, 'index.html', {'date': lust})
+    con = {'lust', lust}
+    return render(req, "index.html", context=con)
+
+
+def detail(req, question_id):
+    return HttpResponse(f'detail :{question_id}')
+
+
+def results(req, question_id):
+    return HttpResponse(f"result: {question_id}")
+
+
+def vote(req, question_id) -> HttpResponse:
+    return HttpResponse(f"vote: {question_id}")
