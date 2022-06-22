efc32e495c1b854499860dc65f64a317447f77ce
chimaek <pipiru100@gmail.com>
2022-06-22 14:03:22 +0900

    add bulma.css

--- first_django_app/first_app/views.py
+++ first_django_app/first_app/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
@@ -6,14 +7,16 @@ from first_app.models import Question
 
 def index(req):
     lust = Question.objects.order_by('pub_date')[:5]
-    # output = ','.join([q.question for q in lust])
-    # return render(req, 'index.html', {'date': lust})
-    con = {'lust', lust}
     return render(req, "index.html", {'lust': lust})
 
 
 def detail(req, question_id):
-    return HttpResponse(f'detail :{question_id}')
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404(f"{question_id} does not exist")
+
+    return render(req, "detail.html", {'question': question})
 
 
 def results(req, question_id):
@@ -22,3 +25,19 @@ def results(req, question_id):
 
 def vote(req, question_id) -> HttpResponse:
     return HttpResponse(f"vote: {question_id}")
+
+
+class me(object):
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.database = 2123
+
+    def generate(self):
+        for i in range(1, self.database):
+            print(i)
+            yield i
