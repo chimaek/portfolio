b17d1b1de736cffe7e3d7146e0aa9312c108222c
seungju.kim <pipiru100@gmail.com>
2024-07-22 14:52:30 +0900

    security init

--- src/main/java/net/chimaek/spring_homepage/config/SecurityConfig.java
+++ src/main/java/net/chimaek/spring_homepage/config/SecurityConfig.java
@@ -1,10 +1,26 @@
 package net.chimaek.spring_homepage.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+            authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/", "/public/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated());
+    return http.build();
+  }
 }
